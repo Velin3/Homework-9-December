dfc02c913b56049eab2ce71d68c9fc85c0247944
Elena <ivanovelens@gmail.com>
2021-12-13 00:22:05 +0300

    StepThree

--- Homework 9 December/ViewController.swift
+++ Homework 9 December/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var countRed: UILabel!
+    @IBOutlet weak var countGreen: UILabel!
+    @IBOutlet weak var countBlue: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
