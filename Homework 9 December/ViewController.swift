44a05caff78eaa14cf8fde2b5f0ed72b8268a67e
Elena <ivanovelens@gmail.com>
2021-12-12 10:49:37 +0300

    Initial Commit

---
+++ Homework 9 December/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 9 December
+//
+//  Created by Елена Иванова on 12/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
